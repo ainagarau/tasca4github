90e1125797fbb85f0bc09d3136a6c81cddb3023b
Aina <ainagarauvillalba@alumnes.iesllucmajor.org>
2023-05-09 12:52:36 +0200

    Primera versió tasca4github

---
+++ tasca4githubexercici1/src/main/java/mavenproject1/tasca4githubexercici1/Tasca4githubexercici1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mavenproject1.tasca4githubexercici1;
+
+/**
+ *
+ * @author ainag
+ */
+public class Tasca4githubexercici1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
