04ba2975d8d76dc1adc21a9d37814a66774fad18
Aina <ainagarauvillalba@alumnes.iesllucmajor.org>
2023-05-15 09:43:37 +0200

    Exercici 1 taules de multiplicar

--- tasca4githubexercici1/src/main/java/mavenproject1/tasca4githubexercici1/Tasca4githubexercici1.java
+++ tasca4githubexercici1/src/main/java/mavenproject1/tasca4githubexercici1/Tasca4githubexercici1.java
@@ -5,6 +5,8 @@
 
 package mavenproject1.tasca4githubexercici1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ainag
@@ -12,6 +14,18 @@ package mavenproject1.tasca4githubexercici1;
 public class Tasca4githubexercici1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.print("Introdueix un nombre entre 1 i el 10:");
+       int nombre= sc.nextInt();
+       
+       if (nombre < 1 || nombre > 10) {
+           System.out.println("Malament! El nombre ha d'estar entre l'1 i el 10");
+           return;
+       }
+       System.out.println("Taula de multiplicar del " + nombre+ ":" );
+       for (int i= 1; i<=10; i++) {
+           System.out.println(nombre+ "x" + i + "="+ (nombre*i));
+       }
     }
 }
